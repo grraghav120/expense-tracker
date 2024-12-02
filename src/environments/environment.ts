f208c150be687b7ca508343942282cd8771d1ac0
Raghav Garg <grraghav120@gmail.com>
2024-12-02 08:40:48 +0530

    Revert "using environment variables"
    This reverts commit 88758aca4e1f4877fb870170238b4f7e8ea0fec8.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:false,
     apiUrl:'https://expense-tracker-t3cs.onrender.com/v1/api/',
-    adminId: process.env['NG_APP_ADMIN_ID']
+    adminId: '6558727029c0dacee0900c6a'
 };
