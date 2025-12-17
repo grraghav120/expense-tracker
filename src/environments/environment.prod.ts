624d9c8cfa2e2ae9ef5f5dcf8db273858bab0a61
Raghav Garg <grraghav120@gmail.com>
2025-12-17 16:47:06 +0530

    env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
-    apiUrl:'https://expense-tracker-t3cs.onrender.com/v1/api/',
+    apiUrl: 'API_URL',
     adminId: '6558727029c0dacee0900c6a'
 };
