ea3f267ccee2c562b7bfcc375402112ccebe709b
Raghav Garg <grraghav120@gmail.com>
2025-12-17 17:04:07 +0530

    weiste url added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production:true,
-    apiUrl: 'API_URL',
-    adminId: '6558727029c0dacee0900c6a'
+    apiUrl:'https://expense-tracker-t3cs.onrender.com/v1/api/',
+    adminId: '6558727029c0dacee0900c6a',
+    WEBSITE_URL: 'https://expense-tracker-admin120.netlify.app/'
 };
